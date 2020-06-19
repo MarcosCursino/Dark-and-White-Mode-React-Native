a5a479b4cb2d426c332fe8ac411842f5aba88441
Marcos <marcos.cursino@hotmail.com>
2020-06-19 11:48:25 -0300

    eslint and prettier installed

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+// import { Container } from './styles';
+
+const src = () => {
+  return <Text>Alooo a</Text>;
+};
+
+export default src;
